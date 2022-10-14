800c8e76fdb738bbc544627d90ed8c2d90365a66
ungquocnhan <nhan09bk@gmail.com>
2022-10-14 16:11:18 +0700

    module 3

---
+++ ss1_database/exercise/ss1_exercise_manage_student.sql
@@ -0,0 +1,36 @@
+create database exercise;
+use exercise;
+
+create table student(
+id int not null primary key,
+name varchar(45) not null,
+age int not null,
+country varchar(45) not null);
+
+create table teacher(
+id int not null primary key,
+name varchar(45) not null,
+age int not null,
+country varchar(45) not null);
+
+create table class(
+id int not null primary key,
+name varchar(45) not null);
+
+insert into class value (1, 'C0722G1'),(2,'C0822G1'),(3, 'C0922G1');
+
+select * from class;
+
+insert into student (id,name,age,country)
+value (2,'Nhân',32,'Việt Nam'),
+	  (3,'Nhân',31,'Việt Nam'),
+      (4,'Nhân',30,'Da Nang'),
+      (5,'Nhân',29,'Da Nang');
+
+select * from student;
+
+update student
+set name = 'Quốc' 
+where id = 3;
+
+delete from student where id = 5;
---
+++ ss1_database/practice/create_database_table.sql
@@ -0,0 +1,7 @@
+create database practice_create_database;
+use practice_create_database;
+create table student(
+id int primary key,
+name varchar(45),
+age int,
+country varchar(45));

582a509d058e29e4b1ac2b366450894b75bec888
ungquocnhan <nhan09bk@gmail.com>
2022-10-16 16:13:44 +0700

    module 3

--- casestudy_database/create_table_casestudy.sql
+++ casestudy_database/create_table_casestudy.sql
@@ -2,7 +2,7 @@ create database case_study_furama;
 use case_study_furama;
 
 create table nhan_vien(
-ma_nhan_vien int,
+ma_nhan_vien int auto_increment,
 ho_ten varchar(45),
 ngay_sinh date,
 so_cmnd varchar(45) unique,
@@ -16,22 +16,22 @@ ma_bo_phan int,
 primary key(ma_nhan_vien));
 
 create table vi_tri(
-ma_vi_tri int,
+ma_vi_tri int auto_increment,
 ten_vi_tri varchar(45),
 primary key(ma_vi_tri));
 
 create table trinh_do(
-ma_trinh_do int,
+ma_trinh_do int auto_increment,
 ten_trinh_do varchar(45),
 primary key(ma_trinh_do));
 
 create table bo_phan(
-ma_bo_phan int,
+ma_bo_phan int auto_increment,
 ten_bo_phan varchar(45),
 primary key(ma_bo_phan));
 
 create table khach_hang(
-ma_khach_hang int,
+ma_khach_hang int auto_increment,
 ma_loai_khach int,
 ho_ten varchar(45),
 ngay_sinh date,
@@ -43,12 +43,12 @@ dia_chi varchar(45),
 primary key(ma_khach_hang));
 
 create table loai_khach(
-ma_loai_khach int,
+ma_loai_khach int auto_increment,
 ten_loai_khach varchar(45),
 primary key(ma_loai_khach));
 
 create table hop_dong(
-ma_hop_dong int,
+ma_hop_dong int auto_increment,
 ngay_lam_hop_dong datetime,
 ngay_ket_thuc datetime,
 tien_dat_coc double,
@@ -58,7 +58,7 @@ ma_dich_vu int,
 primary key(ma_hop_dong));
 
 create table dich_vu(
-ma_dich_vu int,
+ma_dich_vu int auto_increment,
 ten_dich_vu varchar(45),
 dien_tich int,
 chi_phi_thue double,
@@ -73,19 +73,19 @@ dich_vu_mien_phi_di_kem text,
 primary key(ma_dich_vu));
 
 create table kieu_thue(
-ma_kieu_thue int,
+ma_kieu_thue int auto_increment,
 ten_kieu_thue varchar(45),
 primary key(ma_kieu_thue));
 
 create table hop_dong_chi_tiet(
-hop_dong_chi_tiet int,
+hop_dong_chi_tiet int auto_increment,
 ma_hop_dong int,
 ma_dich_vu_di_kem int,
 so_luong int,
 primary key(hop_dong_chi_tiet));
 
 create table dich_vu_di_kem(
-ma_dich_vu_di_kem int,
+ma_dich_vu_di_kem int auto_increment,
 ten_dich_vu_di_kem varchar(45),
 gia double,
 don_vi varchar(45),
@@ -93,6 +93,8 @@ trang_thai varchar(45),
 primary key(ma_dich_vu_di_kem));
 
 create table loai_dich_vu(
-ma_loai_dich_vu int,
+ma_loai_dich_vu int auto_increment,
 ten_loai_dich_vu varchar(45),
 primary key(ma_loai_dich_vu));
+
+

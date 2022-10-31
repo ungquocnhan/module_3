9acdb8c003a714f18ced196deaba0031984eef99
ungquocnhan <nhan09bk@gmail.com>
2022-10-31 21:51:06 +0700

    module 3

---
+++ casestudy_web/create_furama.sql
@@ -0,0 +1,157 @@
+CREATE DATABASE case_study_furama_web;
+USE case_study_furama_web;
+
+CREATE TABLE position (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45)
+);
+
+CREATE TABLE division (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45)
+);
+
+CREATE TABLE education_degree (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45)
+);
+
+CREATE TABLE user (
+username VARCHAR(255) PRIMARY KEY,
+password VARCHAR(255)
+);
+
+CREATE TABLE role (
+role_id INT AUTO_INCREMENT PRIMARY KEY,
+role_name VARCHAR(255)
+);
+
+CREATE TABLE user_role (
+    role_id INT,
+    username VARCHAR(255),
+    PRIMARY KEY (role_id , username),
+    FOREIGN KEY (username)
+        REFERENCES user (username)
+        ON DELETE CASCADE,
+    FOREIGN KEY (role_id)
+        REFERENCES role (role_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45) NOT NULL,
+    day_of_birth DATE NOT NULL,
+    id_card VARCHAR(45) UNIQUE NOT NULL,
+    salary DOUBLE NOT NULL,
+    phone_number VARCHAR(45) UNIQUE NOT NULL,
+    email VARCHAR(45) UNIQUE,
+    address VARCHAR(45),
+    position_id INT,
+    education_degree_id INT,
+    division_id INT,
+    username VARCHAR(255) UNIQUE,
+    FOREIGN KEY (position_id)
+        REFERENCES position (id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (education_degree_id)
+        REFERENCES education_degree (id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (division_id)
+        REFERENCES division (id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (username)
+        REFERENCES user (username)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE customer_type (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45)
+);
+
+CREATE TABLE customer (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45) NOT NULL,
+    day_of_birth DATE NOT NULL,
+    gender BIT(1) NOT NULL,
+    id_card VARCHAR(45) UNIQUE NOT NULL,
+    phone_number VARCHAR(45) UNIQUE NOT NULL,
+    email VARCHAR(45) UNIQUE,
+    address VARCHAR(45),
+    customer_type_id INT,
+    FOREIGN KEY (customer_type_id)
+        REFERENCES customer_type (id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE facility_type (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45)
+);
+
+CREATE TABLE rent_type (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45)
+);
+
+CREATE TABLE facility (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45) NOT NULL,
+    area INT,
+    cost DOUBLE NOT NULL,
+    max_people INT,
+    standard_room VARCHAR(45),
+    description_other_convenience VARCHAR(45),
+    pool_area DOUBLE,
+    number_of_floors INT,
+    facility_free TEXT,
+    rent_type_id INT,
+    facility_type_id INT,
+    FOREIGN KEY (rent_type_id)
+        REFERENCES rent_type (id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (facility_type_id)
+        REFERENCES facility_type (id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE attach_facility (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45) NOT NULL,
+    cost DOUBLE NOT NULL,
+    unit VARCHAR(45) NOT NULL,
+    status VARCHAR(45)
+);
+
+CREATE TABLE contract (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    start_day DATE NOT NULL,
+    end_day DATE NOT NULL,
+    deposit DOUBLE NOT NULL,
+    employee_id INT,
+    customer_id INT,
+    facility_id INT,
+    FOREIGN KEY (employee_id)
+        REFERENCES employee (id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (customer_id)
+        REFERENCES customer (id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (facility_id)
+        REFERENCES facility (id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE contract_detail (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    quantity INT NOT NULL,
+    contract_id INT,
+    attach_facility_id INT,
+    FOREIGN KEY (contract_id)
+        REFERENCES contract (id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (attach_facility_id)
+        REFERENCES attach_facility (id)
+        ON DELETE CASCADE
+);
---
+++ casestudy_web/insert_furama.sql
@@ -0,0 +1,121 @@
+INSERT INTO case_study_furama_web.position (name)
+VALUE ('Quản Lý'),
+	  ('NHân Viên');
+
+INSERT INTO case_study_furama_web.education_degree (name)
+VALUE ('Trung Cấp'),
+      ('Cao Đẳng'),  
+      ('Đại Học'), 
+      ('Sau Đại Học');
+      
+INSERT INTO case_study_furama_web.division (division)
+VALUE ('Sale-Marketing'),
+	  ('Hành Chính'),
+      ('Phục vụ'),
+      ('QUản lý');
+      
+INSERT INTO user (username, password)
+VALUE 	('nguyenan', '1234abcd'),
+		('lebinh', '1234abcd'),
+		('hoyen', '1234abcd'),
+		('votoan', '1234abcd'),
+		('nguyenphat', '1234abcd'),
+		('khucnghi', '1234abcd'),
+		('nguyenha', '1234abcd'),
+		('nguyendong', '1234abcd'),
+		('tonghoang', '1234abcd'),
+		('nguyendao', '1234abcd');
+        
+INSERT INTO role (role_name)
+VALUE();
+
+INSERT INTO user_role (role_id, username)
+VALUE();
+      
+INSERT INTO case_study_furama_web.employee ( name, day_of_birth, id_card, salary, phone_number, email, address, position_id, education_degree_id, division_id) 
+VALUE
+('Nguyễn Văn An', '1970-11-07', '456231786', 10000000, '0901234121', 'annguyen@gmail.Com', '295 Nguyễn Tất Thành, Đà Nẵng', 1, 3, 1),
+('Lê Văn Bình', '1997-04-09', '654231234', 7000000, '0934212314', 'binhlv@gmail.com', '22 Yên Bái, Đà Nẵng', 1, 2, 2),
+('Hồ Thị Yến', '1995-12-12', '999231723', 14000000, '0412352315', 'thiyen@gmail.cOm', 'K234/11 Điện Biên Phủ, Gia Lai', 1, 3, 2),
+('Võ Công Toản', '1980-04-04', '123231365', 17000000, '0374443232', 'toan0404@gMail.com', '77 Hoàng Diệu, Quảng Trị', 1, 4, 4),
+('Nguyễn Bỉnh Phát', '1999-12-09', '454363232', 6000000, '0902341231', 'phatphat@gmail.com', '43 Yên Bái, Đà Nẵng', 2, 1, 1),
+('Khúc Nguyễn An Nghi', '2000-11-08', '964542311', 7000000, '0978653213', 'annghi20@gmail.com', '294 Nguyễn Tất Thành, Đà Nẵng', 2, 2, 3),
+('Nguyễn Hữu Hà', '1993-01-01', '534323231', 8000000, '0941234553', 'nhh0101@gmaiL.com', '4 Nguyễn Chí Thanh, Huế', 2, 3, 2),
+('Nguyễn Hà Đông', '1989-09-03', '234414123', 9000000, '0642123111', 'donghanguyen@gmaIl.coM', '111 HùNg Vương, Hà Nội', 2, 4, 4),
+('Tòng Hoang', '1982-09-03', '256781231', 6000000, '0245144444', 'hoangtong@gmail.com', '213 Hàm Nghi, Đà Nẵng', 2, 4, 4),
+('Nguyễn Công Đạo', '1994-01-08', '755434343', 8000000, '0988767111', 'nguyencongdao12@gmail.com', '6 Hoà Khánh, Đồng Nai', 2, 3, 2);
+
+INSERT INTO case_study_furama_web.customer_type(name)
+VALUE ('Diamond'),
+	  ('Platinium'),
+      ('Gold'),
+      ('Silver'),
+      ('Member');
+
+INSERT INTO case_study_furama_web.customer (name, day_of_birth, gender, id_card, phone_number, email, address, customer_type_id) 
+VALUE
+('Nguyễn Thị Hào', '1970-11-07', 0, '643431213', '0945423362', 'thihao07@gmail.Com', '23 Nguyễn Hoàng, Đà Nẵng', 5),
+('Phạm Xuân Diệu', '1992-08-08', 1, '865342123', '0954333333', 'xuandieu92@gmail.Com', 'K77/22 Thái Phiên, Quảng Trị', 3),
+('Trương Đình Nghệ', '1990-02-27', 1, '488645199', '0373213122', 'nghenhan2702@gmail.coM', 'K323/12 Ông Ích Khiêm, Vinh', 1),
+('Dương Văn Quan', '1981-07-08', 1, '543432111', '0490039241', 'duongquan@gmail.com', 'K453/12 Lê Lợi, Đà Nẵng', 1),
+('Hoàng Trần Nhi Nhi', '1995-12-09', 0, '795453345', '0312345678', 'nhinhi123@gmaIl.Com', '224 Lý Thái Tổ, Gia Lai', 4),
+('Tôn Nữ Mộc Châu', '2005-12-06', 0, '732434215', '0988888844', 'tonnuchau@gmAil.com', '37 Yên Thế, Đà Nẵng', 4),
+('Nguyễn Mỹ Kim', '1984-04-08', 0, '856453123', '0912345698', 'kimcuong84@gmail.com', 'K123/45 Lê Lợi, Hồ Chí Minh', 1),
+('Nguyễn Thị Hào', '1999-04-08', 0, '965656433', '0763212345', 'haohao99@Gmail.com', '55 Nguyễn Văn Linh, Kon Tum', 3),
+('Trần Đại Danh', '1994-07-01', 1, '432341235', '0643343433', 'danhhai99@gmaIl.coM', '24 Lý Thường Kiệt, Quảng Ngãi', 1),
+('Nguyễn Tâm Đắc', '1989-07-01', 1, '344343432', '0987654321', 'dactam@gmail.com', '22 Ngô Quyền, Đà Nẵng', 2);
+
+INSERT INTO case_study_furama_web.facility_type (name)
+VALUE ('year'),
+	  ('month'),
+      ('day'),
+      ('hour');
+      
+INSERT INTO case_study_furama_web.facility_type (name)
+VALUE ('Villa'),
+      ('House'),
+      ('Room');
+      
+INSERT INTO case_study_furama_web.facility (name, area, cost, max_people, standard_room, description_other_convenience, pool_area, number_of_floors, facility_free, rent_type_id, facility_type_id)
+VALUE
+('Villa Beach Front', 25000, 1000000, 10, 'vip', 'Có hồ bơi', 500, 4, null, 3, 1),
+('House Princess 01', 14000, 5000000, 7, 'vip', 'Có thêm bếp nướng', null, 3, null, 2, 2),
+('Room Twin 01', 5000, 1000000, 2, 'normal', 'Có tivi', null, null, '1 Xe máy, 1 Xe đạp', 4, 3),
+('Villa No Beach Front', 22000, 9000000, 8, 'normal', 'Có hồ bơi', 300, 3, null, 3, 1),
+('House Princess 02', 10000, 4000000, 5, 'normal', 'Có thêm bếp nướng', null, 2, null, 3, 2),
+('Room Twin 02', 3000, 900000, 2, 'normal', 'Có tivi', null, null, '1 Xe máy', 4, 3);
+
+INSERT INTO case_study_furama_web.attach_facility (name, cost, unit, status) 
+VALUE
+('Karaoke', 10000, 'giờ', 'tiện nghi, hiện tại'),
+('Thuê xe máy', 10000, 'chiếc', 'hỏng 1 xe'),
+('Thuê xe đạp', 20000, 'chiếc', 'tốt'),
+('Buffet buổi sáng', 15000, 'suất', 'đầy đủ đồ ăn, tráng miệng'),
+('Buffet buổi trưa', 90000, 'suất', 'đầy đủ đồ ăn, tráng miệng'),
+('Buffet buổi tối', 16000, 'suất', 'đầy đủ đồ ăn, tráng miệng');
+
+INSERT INTO case_study_furama_web.contract (start_day, end_day, deposit, employee_id, customer_id, facility_id) 
+VALUE
+('2020-12-08', '2020-12-08', 0, 3, 1, 3),
+('2020-07-14', '2020-07-21', 200000, 7, 3, 1),
+('2021-03-15', '2021-03-17', 50000, 3, 4, 2),
+('2021-01-14', '2021-01-18', 100000, 7, 5, 5),
+('2021-07-14', '2021-07-15', 0, 7, 2, 6),
+('2021-06-01', '2021-06-03', 0, 7, 7, 6),
+('2021-09-02', '2021-09-05', 100000, 7, 4, 4),
+('2021-06-17', '2021-06-18', 150000, 3, 4, 1),
+('2020-11-19', '2020-11-19', 0, 3, 4, 3),
+('2021-04-12', '2021-04-14', 0, 10, 3, 5),
+('2021-04-25', '2021-04-25', 0, 2, 2, 1),
+('2021-05-25', '2021-05-27', 0, 7, 10, 1);
+
+INSERT INTO case_study_furama_web.contract_detail (quantity, contract_id, attach_facility_id) 
+VALUE
+(5, 2, 4),
+(8, 2, 5),
+(15, 2, 6),
+(1, 3, 1),
+(11, 3, 2),
+(1, 1, 3),
+(2, 1, 2),
+(2, 12, 2);
